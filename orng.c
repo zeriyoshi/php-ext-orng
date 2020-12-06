ee22a5a5f1c24ca8eb0832c99d9136e3988c3be2
zeriyoshi <zeriyoshi@gmail.com>
2020-12-06 17:40:46 +0900

    [CI skip] Add LICENCE

--- orng.c
+++ orng.c
@@ -1,5 +1,21 @@
-/* orng extension for PHP */
-
+/*
+  +----------------------------------------------------------------------+
+  | ORNG - PHP Objective RNG Extension                                   |
+  +----------------------------------------------------------------------+
+  | Copyright (c) zeriyoshi                                              |
+  | Copyright (c) The PHP Group                                          |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: zeriyoshi <zeriyoshi@gmail.com>                              |
+  +----------------------------------------------------------------------+
+*/
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
